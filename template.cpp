8ec604e809780684e1430ce44f4eb405acc25fc1
roirepus <rajsinsinwar159@gmail.com>
2024-03-02 15:31:24 +0530

    repository to host contest codes

---
+++ template.cpp
@@ -0,0 +1,15 @@
+// Problem: $(PROBLEM)
+// Contest: $(CONTEST)
+// Judge: $(JUDGE)
+// URL: $(URL)
+// Memory Limit: $(MEMLIM)
+// Time Limit: $(TIMELIM)
+// Start: $(DATE)
+
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "This is a template file" << endl;
+  cerr << "Problem name is $(PROBLEM)" << endl;
+  return 0;
+}
